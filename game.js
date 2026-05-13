65d316fbf5a5e669d88ad2a56d29d8e1497f81e7
Rock Ritesh <riteshgangwar761@gmail.com>
2026-05-13 11:24:22 +0530

    Add initial game files and configuration for Tic Tac Toe

---
+++ game.js
@@ -0,0 +1,34 @@
+
+
+
+// // let boxes = document.qureyselectorall(".box");
+// // let resetbtn = document.querySelector("#button");
+
+// // let trunO = true;
+
+// // const winpatterns = [
+// //     [0, 1, 2]
+// //     [0, 3, 6]
+// //     [0, 4, 8]
+// //     [1, 4, 7]
+// //     [2, 5, 8]
+// //     [2, 4, 6]
+// //     [3, 4, 5]
+// //     [6, 7, 8]
+// // ]
+
+// // boxes.forEach((box) => {
+// //     box.addEventlistener("click", () => {
+// //         console.log("box was clicked");
+// //         box.innretext="Ab";
+// //     })
+// // }
+// // )
+
+// // Array Data Structure
+// let numbers = [10, 20, 30];
+
+// // Simple Algorithm
+// for(let i = 0; i < numbers.length; i++) {
+//    console.log(numbers[i]);
+// }
